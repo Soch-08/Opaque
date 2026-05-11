3900e23563d3e4222b8caa826af638b5ce512af7
Soch Prosper <145940110+Soch-08@users.noreply.github.com>
2026-05-11 11:20:27 +0100

    Fix indentation and formatting in hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,12 +7,11 @@ module.exports = {
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: [process.env.PRIVATE_KEY],
-      timeout: 120000,
-    },
+      accounts: [process.env.PRIVATE_KEY]
+    }
   },
 
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
