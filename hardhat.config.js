87db8510cd891498ca74d108d7eff7af160f076c
Soch Prosper <145940110+Soch-08@users.noreply.github.com>
2026-05-11 11:41:47 +0100

    Refactor Hardhat config to use environment variables

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,23 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+
+if (!INFURA_API_KEY) {
+  throw new Error("Missing INFURA_API_KEY in environment variables");
+}
+
 module.exports = {
   solidity: "0.8.24",
 
   networks: {
     sepolia: {
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
-      accounts: ["4dea6ba3801759c12cd8f0fd9b7d2041ee1fb37bfd27cbfc54b508328b16bf25"]
-    }
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [process.env.PRIVATE_KEY],
+    },
   },
 
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY
-  }
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
